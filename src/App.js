5b5ec2751b3bd89757256e7c8b3d4bba5a160f03
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-06-21 00:22:10 -0300

    Refatora o componente App: move o cabeçalho para um novo componente Header e organiza estilos em arquivos separados

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import './App.css';
-import Logo from './componentes/logo';
-import perfil from './imagens/perfil.svg';
-import sacola from './imagens/sacola.svg';
-
-const textoOpcoes = [
-  "Categorias",
-  "Minha Estante",
-  "Favoritos"
-]
-const icones =[perfil, sacola];
- 
+import Header from './componentes/header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Logo></Logo> 
-        <ul className='opcoes'>
-          {textoOpcoes.map((texto)=>(
-            <li className='opcao'><p>{texto}</p></li>
-          ))}
-        </ul>
-
-        <ul className='icones'> 
-          {icones.map((icone)=>(
-            <li className='icone'><img src={icone}></img></li>
-          ))}
-        </ul>  
-      </header>
+      <Header/>
     </div>
   );
 }
