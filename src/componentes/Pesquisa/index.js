36a4a243a9314553eb03cdf04c1c3f4a22093b8f
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-06-27 20:54:47 -0300

    adiciona estilização ao componente Resultado e exibe livros pesquisados

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -22,10 +22,26 @@ const Subtitulo = styled.h3`
     font-weight: 500;
     margin-bottom: 40px;
 `
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
 
 function Pesquisa(){
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
-    console.log(livrosPesquisados)
+    
     return(
         <PesquisaContainer>
            <Titulo>Já sabe poe onde começar ?</Titulo>
@@ -38,7 +54,15 @@ function Pesquisa(){
                 setLivrosPesquisados(resultadoPesquisa)
         }}
            />
-           
+           { livrosPesquisados.map( livro =>(
+            <Resultado>
+                <div>
+                    <p>{livro.nome}</p> 
+                    <img src={livro.src}></img> 
+                </div> 
+                
+            </Resultado>
+           ))}
         </PesquisaContainer>
     )
 }
