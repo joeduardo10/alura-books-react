9ce4f2eed5a70888168518ba7e15d414f178ed0c
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-06-30 22:47:57 -0300

    adiciona componente CardRecomenda e integra ao componente UltimosLancamentos

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,5 +1,7 @@
 import { livros } from './dadosUltimosLancamentos'
 import { Titulo } from '../Titulo'
+import CardRecomenda from '../CardRecomenda'
+import imagemLivro from "../../imagens/livro2.png"
 import styled from 'styled-components'
 
 const UltimosLancamentosContainer = styled.section`
@@ -23,13 +25,21 @@ function UltimosLancamentos() {
             <Titulo
             cor="#EB9B00"
             tamanhoFonte="36px"
-            
-        >ÚLTIMOS LANÇAMENTOS</Titulo>
+        >
+        ÚLTIMOS LANÇAMENTOS
+        </Titulo>
             <NovosLivrosContainer>
                 { livros.map( livro => (
                     <img src={livro.src}/>
                 ) ) }
             </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação com a plataforma Google"
+                img={imagemLivro}
+            />
+
         </UltimosLancamentosContainer>
     )
 }
